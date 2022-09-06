fac1333e5941b3e9f924d4c97acf555380cf18fb
Magnus Hirth <magnus.hirth@gmail.com>
2022-09-06 20:38:50 +0200

    Properly ignore files and folders

--- .scripts/filelist.py
+++ .scripts/filelist.py
@@ -2,14 +2,14 @@
 
 from pathlib import Path
 
-IGNORE_FILE = '.meta/ignore.txt'
+IGNORE_FILE = 'meta/ignore.txt'
 IGNORE_LIST = Path(IGNORE_FILE).read_text().splitlines(False)
 
 
 def list_files(path: Path):
     for file in path.iterdir():
         # Ignore all hidden files and folders
-        if file.match('.*'):
+        if file.match('.*') or str(file) in IGNORE_LIST:
             continue
         elif file.is_file():
             yield str(file)
@@ -19,8 +19,6 @@ def list_files(path: Path):
 
 def main():
     for file in list_files(Path('.')):
-        if file in IGNORE_LIST:
-            continue
         print(file)
 
 
