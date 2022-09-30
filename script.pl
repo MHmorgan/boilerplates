b22cb0b04de22f29037f269bd89b7c6141046ea0
Magnus Hirth <magnus.hirth@gmail.com>
2022-09-30 16:44:30 +0200

    Update script.pl to a more usefull boilerplate.

--- script.pl
+++ script.pl
@@ -1,100 +1,45 @@
-#!/usr/bin/env perl5.30
+#!/usr/bin/env perl
 
 use v5.30.0;
 use utf8;
+use strict;
 use warnings;
+
+BEGIN { push @INC, "$ENV{HOME}/lib"; }
+
 use open qw(:std :utf8);
-
+use common;
 use Getopt::Long;
-use Pod::Usage;
 
-BEGIN {
-    push @INC, "$ENV{HOME}/lib";
+
+my $HELP = !(scalar @ARGV);
+my $VERBOSE = 0;
+
+GetOptions(
+    'help|h'    => \$HELP,
+    'verbose|v' => \$VERBOSE,
+);
+
+if ($HELP) {
+    print while (<DATA>);
+    exit;
 }
 
-use common;
 
-sub foo {{{
+sub foo {
     my ($name, $n) = @_;
     $n = 1 unless $n;
     $name = 'foo' unless $name;
     say "Hello $name!" while $n-- > 0;
-}}}
+}
 
 
-################################################################################
-# main
-#{{{
-
-my $MAN = 0;
-my $HELP = 0;
-my $FOO = 'foo';
-
-GetOptions('help|h' => \$HELP,
-           'foo'    => \$FOO,
-           'man'    => \$MAN)
-or pod2usage(2);
-
-pod2usage( -exitval  => 0,
-           -verbose  => 2) if $MAN;
-pod2usage( -verbose  => 99,
-           -sections => "SYNOPSIS|Commands|OPTIONS")
-if $HELP || !(scalar @ARGV);
-
-sub main {{{
-    my $cmd = shift;
-    my $args = join '", "', @_;
-    my $res = eval "$cmd(\"$args\")";
-	say $res if defined $res;
-}}}
-
-main @ARGV;
-#}}}
+say "Hello world!";
+say "Suddenly the cave collapses!" if $VERBOSE;
 
 
-################################################################################
-#                                                                              #
-# Documentation
-#                                                                              #
-################################################################################
+__DATA__
+Usage: foo [-v] [-h]
 
-__END__
+Do something cool!
 
-=head1 NAME
-
-rogu - Roger's weird offspring.
-
-=head1 SYNOPSIS
-
-rogu [options] <command> [args...]
-
-=head1 Commands
-
-=over 8
-
-=item B<foo> [name] [num]
-
-Foo-greet!
-
-=back
-
-=head1 OPTIONS
-
-=over 8
-
-=item B<--help>
-
-Print a brief help message and exits.
-
-=item B<--man>
-
-Prints the manual page and exits.
-
-=back
-
-=head1 DESCRIPTION
-
-B<This program> will read the given input file(s) and do something
-useful with the contents thereof.
-
-=cut
